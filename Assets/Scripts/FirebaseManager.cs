4029c621b99591f66d008071779aec39b8fcf52c
AlanW95 <alan.williams.1995@gmail.com>
2021-12-13 17:53:50 +0000

    13/12/21_01 Update

--- Assets/Scripts/FirebaseManager.cs
+++ Assets/Scripts/FirebaseManager.cs
@@ -101,12 +101,12 @@ public class FirebaseManager : MonoBehaviour
 
     public void LoginButton()
     {
-
+        StartCoroutine(LoginLogic(loginEmail.text, loginPassword.text));
     }
 
     public void RegisterButton()
     {
-
+        StartCoroutine(RegisterLogic(registerUsername.text, registerEmail.text, registerPassword.text, registerConfirmPassword.text));
     }
 
     private IEnumerator LoginLogic(string _email, string _password)
